bbe196ce4c981e47529032696bec5c1cda1cd97d
krontogiannis <rodoconst@hotmail.com>
2013-10-14 18:22:47 +0300

    Added a standalone sample project

---
+++ MBrace.Demos/MBrace.Standalone/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Nessos.MBrace.Client
+
+[<EntryPoint>]
+let main argv =
+    MBraceSettings.MBracedExecutablePath <- @"C:\Program Files (x86)\MBrace\bin\mbraced.exe"
+    MBraceSettings.StoreProvider <- LocalFS
+
+    let runtime = MBrace.InitLocal 3 
+
+    let x = runtime.Run <@ cloud { return 1 + 1 } @>
+    
+    x // return an integer exit code
