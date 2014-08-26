3ea3a747d9ac830c00261a21089056617794c31d
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-08-27 00:53:23 +0300

    add type provider demo

---
+++ Demo.Scripts/Parallel/type providers.fsx
@@ -0,0 +1,22 @@
+﻿#load "../packages/MBrace.Runtime.0.5.4-alpha/bootstrap.fsx" 
+
+open Nessos.MBrace
+open Nessos.MBrace.Client
+
+#r "../packages/FSharp.Data.2.0.10/lib/net40/FSharp.Data.dll"
+
+open FSharp.Data
+
+let runtime = MBrace.InitLocal 3
+
+[<Cloud>]
+let atmCount () = cloud {
+    let wb = WorldBankData.GetDataContext()
+    let countries = wb.Regions.``European Union``.Countries |> Seq.toArray
+    return! 
+        countries 
+        |> Array.map (fun c -> cloud { return c.Name, c.Indicators.``ATMs per 100,000 adults``.Values |> Seq.tryPick Some }) 
+        |> Cloud.Parallel
+}
+
+runtime.Run <@ atmCount () @>
