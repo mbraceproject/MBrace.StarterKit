9b16bf447eb46d149840a311a575a8e0a1447894
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-08-27 01:17:30 +0300

    fix paths

--- Demo.Scripts/Parallel/type providers.fsx
+++ Demo.Scripts/Parallel/type providers.fsx
@@ -1,9 +1,9 @@
-﻿#load "../packages/MBrace.Runtime.0.5.4-alpha/bootstrap.fsx" 
+﻿#load "../../packages/MBrace.Runtime.0.5.4-alpha/bootstrap.fsx" 
 
 open Nessos.MBrace
 open Nessos.MBrace.Client
 
-#r "../packages/FSharp.Data.2.0.10/lib/net40/FSharp.Data.dll"
+#r "../../packages/FSharp.Data.2.0.10/lib/net40/FSharp.Data.dll"
 
 open FSharp.Data
 
@@ -12,10 +12,10 @@ let runtime = MBrace.InitLocal 3
 [<Cloud>]
 let atmCount () = cloud {
     let wb = WorldBankData.GetDataContext()
-    let countries = wb.Regions.``European Union``.Countries |> Seq.toArray
+    let countries = wb.Regions.``European Union``.Countries
     return! 
         countries 
-        |> Array.map (fun c -> cloud { return c.Name, c.Indicators.``ATMs per 100,000 adults``.Values |> Seq.tryPick Some }) 
+        |> Seq.map (fun c -> cloud { return c.Name, c.Indicators.``ATMs per 100,000 adults``.Values |> Seq.tryPick Some }) 
         |> Cloud.Parallel
 }
 
