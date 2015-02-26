c140116216f131d18afe18ce4360ca87103505f6
krontogiannis <rodoconst@hotmail.com>
2015-02-26 18:55:34 +0200

    Fix commit

---
+++ MBraceAzureRole/WorkerRole.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using MBrace.Azure.Runtime;
+using MBrace.Azure.Runtime.Common;
+
+namespace MBraceAzureRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private Service _svc;
+        private Configuration _config;
+
+        public override void Run()
+        {
+            _svc.Start();
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections
+            ServicePointManager.DefaultConnectionLimit = 512;
+
+            // Increase disk quota for mbrace filesystem cache.
+            string customTempLocalResourcePath = RoleEnvironment.GetLocalResource("LocalMBraceCache").RootPath;
+            Environment.SetEnvironmentVariable("TMP", customTempLocalResourcePath);
+            Environment.SetEnvironmentVariable("TEMP", customTempLocalResourcePath);
+
+            bool result = base.OnStart();
+
+            _config = Configuration.Default
+                        .WithStorageConnectionString(CloudConfigurationManager.GetSetting("MBrace.StorageConnectionString"))
+                        .WithServiceBusConnectionString(CloudConfigurationManager.GetSetting("MBrace.ServiceBusConnectionString"));
+
+            _svc = new Service(_config);
+            _svc.AttachLogger(new CustomLogger(s => Trace.WriteLine(String.Format("{0} : {1}", DateTime.UtcNow, s))));
+
+            RoleEnvironment.Changed += RoleEnvironment_Changed;
+
+            return result;
+        }
+
+        void RoleEnvironment_Changed(object sender, RoleEnvironmentChangedEventArgs e)
+        {
+            foreach (var item in e.Changes.OfType<RoleEnvironmentTopologyChange>())
+            {
+                if (item.RoleName == RoleEnvironment.CurrentRoleInstance.Role.Name)
+                {
+                    // take any action needed on instance count modification; gracefully shrink etc
+                }
+            }
+
+            foreach (var item in e.Changes.OfType<RoleEnvironmentConfigurationSettingChange>())
+            {
+                if (item.ConfigurationSettingName == "MBrace.ServiceBusConnectionString"
+                    || item.ConfigurationSettingName == "MBrace.StorageConnectionString")
+                {
+                    _config = Configuration.Default
+                                .WithStorageConnectionString(CloudConfigurationManager.GetSetting("MBrace.StorageConnectionString"))
+                                .WithServiceBusConnectionString(CloudConfigurationManager.GetSetting("MBrace.ServiceBusConnectionString"));
+                    _svc.Stop();
+                    _svc.Configuration = _config;
+                    _svc.Start();
+                }
+            }
+        }
+
+        public override void OnStop()
+        {
+            base.OnStop();
+            _svc.Stop();
+        }
+    }
+}
