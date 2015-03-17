45c93b7d3a0ed51ef3f1e0aecbc1709e3462e38c
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-03-18 00:33:12 +0200

    update scripts

--- azure/CustomCloudService/MBraceAzureClient/client.fsx
+++ azure/CustomCloudService/MBraceAzureClient/client.fsx
@@ -4,26 +4,30 @@ open MBrace
 open MBrace.Azure
 open MBrace.Azure.Client
 
+
+// configuration object used for connecting with your Azure cluster
 let config = 
     { Configuration.Default with
         StorageConnectionString = "your connection string"
         ServiceBusConnectionString = "your connection string" }
 
 
+// gets a runtime handle to runtime of provided configuration
 let runtime = Runtime.GetHandle(config)
 runtime.AttachClientLogger(new ConsoleLogger()) 
 
 
-runtime.ShowWorkers()
-runtime.ShowProcesses()
+runtime.ShowWorkers() // prints all workers in cluster
+runtime.ShowProcesses() // prints all processes running or completed in cluster
 
+// create a trivial process that returns a string
 let helloJob = runtime.CreateProcess(cloud { return "Hello world"})
 helloJob.AwaitResult()
 
 
+// create a process that executes 10 workflows in parallel in the cluster
 let parallelJob =
-    [1..10]
-    |> List.map (fun i -> cloud { return i * i})
+    [ for i in 1 .. 10 -> cloud { return i * i } ]
     |> Cloud.Parallel
     |> runtime.CreateProcess
 
@@ -32,7 +36,7 @@ parallelJob.ShowInfo()
 parallelJob.AwaitResult()
 
 
-
+// creates a process that executes a computation on every worker on the cluster in parallel
 let ps = 
     Cloud.ParallelEverywhere(cloud { return System.Environment.MachineName })
     |> runtime.CreateProcess
