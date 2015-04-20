c54cf8148bcc8df1dd8dd503d717bbb22dd4460a
isaacabraham <isaac.abraham@gmail.com>
2015-04-20 17:08:20 +0100

    Help guide the resolution of Azure-specific assemblies.

--- azure/HandsOnTutorial/credentials.fsx
+++ azure/HandsOnTutorial/credentials.fsx
@@ -1,6 +1,12 @@
-﻿#load "../../packages/MBrace.Azure.Standalone/MBrace.Azure.fsx"
-#r "../../packages/Streams/lib/net45/Streams.Core.dll"
-#r "../../packages/MBrace.Flow/lib/net45/MBrace.Flow.dll"
+﻿#I "../../packages"
+#load "MBrace.Azure.Standalone/MBrace.Azure.fsx"
+#r @"Streams/lib/net45/Streams.Core.dll"
+#r @"MBrace.Flow/lib/net45/MBrace.Flow.dll"
+#I @"..\..\packages\Newtonsoft.Json\lib\net45"
+#I @"..\..\packages\Microsoft.Data.Edm\lib\net40"
+#I @"..\..\packages\Microsoft.Data.Services.Client\lib\net40"
+#I @"..\..\packages\Microsoft.Data.OData\lib\net40"
+#I @"..\..\packages\System.Spatial\lib\net40"
 
 namespace global
 
