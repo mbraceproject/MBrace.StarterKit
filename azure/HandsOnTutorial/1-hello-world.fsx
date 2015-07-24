ceae22a2fd1629c0fb09d86313afa62b7a6c3cde
Don Syme <donsyme@fastmail.fm>
2015-07-24 18:49:17 +0100

    update hello world

--- azure/HandsOnTutorial/1-hello-world.fsx
+++ azure/HandsOnTutorial/1-hello-world.fsx
@@ -2,19 +2,6 @@
 #load "credentials.fsx"
 
 
-(**
-# Your First Cloud Work
-
-First you send a simple computation to an mbrace cluster using F# Interactive scripting.
-You can also send computations from a compiled F# project, though using scripting is very 
-common with MBrace.
-
-A guide to creating the cluster is [here](https://github.com/mbraceproject/MBrace.StarterKit/blob/master/azure/brisk-tutorial.md#get-started-with-brisk).
-
-> NOTE: Before running, build this solution to get the required nuget packages, and edit credentials.fsx to enter your Azure connection strings.
-
- **)
-
 open System
 open System.IO
 open MBrace.Core
@@ -22,22 +9,28 @@ open MBrace.Azure
 open MBrace.Azure.Client
 open MBrace.Flow
 
-
 (**
+# Your First 'Hello World' Computation with MBrace.Azure
+
+First you send a simple computation to an mbrace cluster using F# Interactive scripting.
+You can also send computations from a compiled F# project, though using scripting is very 
+common with MBrace.
+
+A guide to creating a cluster is [here](http://www.m-brace.net/#try).
+
+> NOTE: Before running, build this solution to get the required nuget packages, and edit credentials.fsx to enter your Azure connection strings.
 
 First connect to the cluster using a configuration to bind to your storage and service bus on Azure.
-
-Before running, edit credentials.fsx to enter your connection strings.
 *)
 let cluster = Runtime.GetHandle(config)
 
-(** Optionally, attach console logger to client object *)
+// Optionally, attach console logger to client object 
 cluster.AttachClientLogger(new ConsoleLogger())
 
-(** You can connect to the cluster and get details of the workers in the pool: *)
+// You can connect to the cluster and get details of the workers in the pool:
 cluster.ShowWorkers()
 
-(** You can view the history of processes: *)
+// You can view the history of processes:
 cluster.ShowProcesses()
 
 (** Now execute your first cloud workflow and get a handle to the running job: *)
@@ -45,10 +38,10 @@ let job =
     cloud { return "Hello world!" } 
     |> cluster.CreateProcess
 
-(** You can evaluate helloWorldProcess to get details on it: *)
+// You can evaluate helloWorldProcess to get details on it.
 let isJobComplete = job.Completed
 
-(** Block until the result is computed by the cluster: *)
+// Block until the result is computed by the cluster
 let text = job.AwaitResult()
 
 (** Alternatively we can do this all in one line: *)
@@ -56,17 +49,24 @@ let quickText =
     cloud { return "Hello world!" } 
     |> cluster.Run
 
-(** You can test a workflow by running locally using async semantics: *)
+(** To check that you are running in the cloud, compre
+a workflow by running locally (using async semantics) with one
+using remote execution: *)
 let localResult =
     cloud { printfn "hello, world" ; return Environment.MachineName }
     |> cluster.RunLocally
 
-(** Now compare the behaviour against remote execution: *)
 let remoteResult =
     cloud { printfn "hello, world" ; return Environment.MachineName }
     |> cluster.Run
 
-(** This can be used to clear all process records in the cluster: *)
+(** 
+
+## Controlling the Cluster
+
+In case you run into trouble, this can be used to clear all process 
+records in the cluster: 
+*)
 
 // cluster.ClearAllProcesses()
 
