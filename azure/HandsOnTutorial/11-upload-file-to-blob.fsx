13d7fdd8ddf914bc0c81659fcde415e78849a63d
jasonw <jasonweiyi@gmail.com>
2015-08-03 11:02:35 +0100

    Updated a tutorial.

--- azure/HandsOnTutorial/11-upload-file-to-blob.fsx
+++ azure/HandsOnTutorial/11-upload-file-to-blob.fsx
@@ -7,12 +7,6 @@ open MBrace.Core
 open MBrace.Store
 open MBrace.Azure
 open MBrace.Azure.Client
-open MBrace.Flow
-
-open Microsoft.WindowsAzure
-open Microsoft.WindowsAzure.Storage
-open Microsoft.WindowsAzure.Storage.Auth
-open Microsoft.WindowsAzure.Storage.Blob
 
 (**
 This tutorial illustrates how to upload local files to Azure blob storage and 
@@ -28,7 +22,7 @@ cluster.ShowProcesses()
 cluster.ShowWorkers()
 
 
-(** Create a tmp file locally, wrote some random content, and return the file path. *)
+(** Now you define functions to create and remove temp files. *)
 let CreateTempFile() =    
     let tmpFile = Path.GetTempFileName()
     use sw = new StreamWriter(tmpFile)
@@ -40,40 +34,29 @@ let CreateTempFile() =
 let DeleteTempFile path =
     File.Delete path
 
-
 (** Create a local temp file. *)
 let tmpFile = CreateTempFile()
 
-// Using Azure SDK to create a tmp container in Azure blob.
-let containerName = "tmp"
-let storageAccount = CloudStorageAccount.Parse myStorageConnectionString
-let blobClient = storageAccount.CreateCloudBlobClient()
-let container = blobClient.GetContainerReference(containerName)
-container.CreateIfNotExists()
 
-
-(** 
-Upload the temp file to the tmp container. Note that this expression 
-has to be run locally using cluster.RunLocally because it accesses local paths.  
-*)
+(** Next, you upload the created file to the tmp container in Azure blob storage. The tmp container
+will be created if it does not exist. Note the use of the local {...} expression and the cluster.RunLocally method:  
+the uploading has to be run locally because it accesses a local path. *)
 let cFile = 
-    CloudFile.Upload(tmpFile, sprintf "%s/%s" containerName (Path.GetFileName tmpFile)) 
+    local {
+        return! CloudFile.Upload(tmpFile, sprintf "tmp/%s" (Path.GetFileName tmpFile))     
+    }    
     |> cluster.RunLocally
 
-(** Delete the local temp file *)
+(** After uploading the file, you remove the local file. *)
 DeleteTempFile tmpFile
 
-(** Cound the lines of the uploaded file in the MBrace cluster. *)
+(** At last, you count the lines of the uploaded file in the MBrace cluster. This cloud expression runs in the MBrace cluster. *)
 let lines = 
-    CloudFile.ReadAllLines cFile.Path 
+    cloud {
+        let! lines = CloudFile.ReadAllLines cFile.Path 
+        return lines.Length
+    } 
     |> cluster.Run
-    |> Array.length
 
-
-
-
-
-
-
-
-
+(** In this tutorial, you've learnt how to upload local files into Azure blob storage and then
+process the uploaded files in the MBrace cluster. *)
