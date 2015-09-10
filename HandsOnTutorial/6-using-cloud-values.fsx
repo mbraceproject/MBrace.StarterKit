eef8f88238839760b5ea6866058a2b040bfb6a9f
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-09-10 19:07:59 +0300

    update CloudValue tutorial

--- HandsOnTutorial/6-using-cloud-values.fsx
+++ HandsOnTutorial/6-using-cloud-values.fsx
@@ -31,7 +31,7 @@ If you wish to _persist_ data in the cloud - for example, if it is too big
 to upload multiple times - then you can use one or more of the
 cloud data constructs that MBrace provides. 
  
-Note you can alternatively you use any existing cloud storage 
+Note you can alternatively use any existing cloud storage 
 APIs or SDKs you already have access to. For example, if you wish you 
 can read/write using the .NET Azure storage SDKs directly rather than 
 using MBrace primitives.
@@ -42,7 +42,7 @@ https://azure.microsoft.com/en-us/documentation/articles/storage-use-azcopy/
 You can manage storage using the "azure" command line tool, see
 https://azure.microsoft.com/en-us/documentation/articles/xplat-cli/
 
- Before running, edit credentials.fsx to enter your connection strings.
+Before running, edit credentials.fsx to enter your connection strings.
  
 *)
  
@@ -50,7 +50,7 @@ https://azure.microsoft.com/en-us/documentation/articles/xplat-cli/
 let data = String.replicate 10000 "The quick brown fox jumped over the lazy dog\r\n" 
 
 
-(** Upload the data to blob storagand return a handle to the stored data *)
+(** Upload the data to blob storage and return a handle to the stored data *)
 let persistedCloudData = 
     cloud { let! cell = CloudValue.New data 
             return cell }
@@ -61,63 +61,3 @@ let lengthOfData =
     cloud { let! data = CloudValue.Read persistedCloudData 
             return data.Length }
     |> cluster.RunOnCloud
-
-
-(** 
-
-//# Using Cloud Arrays
-//
-//Next persist an array of data (each element a tuple).
-//
-//Here is the data we're going to upload, it's an array of 1000 tuples: *) 
-//let dataGen = 
-//    [| for i in 1 .. 1000 do 
-//        let text = sprintf "%d quick brown foxes jumped over %d lazy dogs." i (2*i + 1)
-//        yield (i, text) |]
-//
-//(** 
-//Upload it as a CloudArray; a CloudArray is a CloudValue of a collection.
-//*)
-//let cloudArray = 
-//    cloud {
-//        let! seq = CloudValue.NewArray dataGen
-//        return seq
-//    } |> cluster.RunOnCloud
-//
-//(** For this sample we only read the 10 first elements from store: *)
-//let first10 = Seq.take 10 cloudArray
-//
-//(** The next example runs a cloud job that aggregate all elements to an array
-//and returns the results to the server: *) 
-//let allData = 
-//    cloud {
-//        let! array = cloudArray.GetValueAsync()
-//        return array
-//    } |> cluster.RunOnCloud
-//
-//
-//(** The next sample shows how a collection of cloud sequences can be used
-//as input into a CloudFlow data flow. Each cloud sequence can be used as a 
-//partition of data flowing into the CloudFlow.
-//*)
-//
-//let cloudArrays = 
-//    [ for i in 1 .. 10 -> 
-//        cloud {
-//            let! seq = CloudValue.NewArray dataGen
-//            return seq
-//        } ]
-//    |> Cloud.Parallel
-//    |> cluster.RunOnCloud
-//
-//let results = 
-//    cloudArrays
-//      |> CloudFlow.OfCloudArrays
-//      |> CloudFlow.countBy (fun (k,v) -> k)
-//      |> CloudFlow.toArray
-//      |> cluster.RunOnCloud
-//
-//(** In this tutorial, you've learned how to persist data values into
-//cloud storage using the `CloudValue` and `CloudSequence` types.
-//Continue with further samples to learn more about the
-//MBrace programming model.  *)
