a5b250827a5471a4c88364cce71c36415bf6a89d
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-10-20 14:30:58 +0300

    add wordcount example

---
+++ HandsOnTutorial/200-wordcount.fsx
@@ -0,0 +1,95 @@
+﻿(*** hide ***)
+#load "ThespianCluster.fsx"
+//#load "AzureCluster.fsx"
+
+// Note: Before running, choose your cluster version at the top of this script.
+// If necessary, edit AzureCluster.fsx to enter your connection strings.
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Text.RegularExpressions
+
+open MBrace.Core
+open MBrace.Flow
+
+// Initialize client object to an MBrace cluster
+let cluster = Config.GetCluster() 
+
+(**
+
+# Simple WordCount
+
+*)
+
+#load "lib/textfiles.fsx"
+
+type WordFrequency = string * int64
+type WordCount = WordFrequency []
+
+let private wordRegex = new Regex(@"[\W]+", RegexOptions.Compiled)
+/// Regex word tokenizer
+let splitToWords (line : string) = wordRegex.Split line
+
+/// normalize word
+let normalize (word : string) = word.Trim().ToLower()
+
+/// words ignored by wordcount
+let private noiseWords = 
+    [|
+        "a"; "about"; "above"; "all"; "along"; "also"; "although"; "am"; "an"; "any"; "are"; "aren't"; "as"; "at";
+        "be"; "because"; "been"; "but"; "by"; "can"; "cannot"; "could"; "couldn't"; "did"; "didn't"; "do"; "does"; 
+        "doesn't"; "e.g."; "either"; "etc"; "etc."; "even"; "ever";"for"; "from"; "further"; "get"; "gets"; "got"; 
+        "had"; "hardly"; "has"; "hasn't"; "having"; "he"; "hence"; "her"; "here"; "hereby"; "herein"; "hereof"; 
+        "hereon"; "hereto"; "herewith"; "him"; "his"; "how"; "however"; "I"; "i.e."; "if"; "into"; "it"; "it's"; "its";
+        "me"; "more"; "most"; "mr"; "my"; "near"; "nor"; "now"; "of"; "onto"; "other"; "our"; "out"; "over"; "really"; 
+        "said"; "same"; "she"; "should"; "shouldn't"; "since"; "so"; "some"; "such"; "than"; "that"; "the"; "their"; 
+        "them"; "then"; "there"; "thereby"; "therefore"; "therefrom"; "therein"; "thereof"; "thereon"; "thereto"; 
+        "therewith"; "these"; "they"; "this"; "those"; "through"; "thus"; "to"; "too"; "under"; "until"; "unto"; "upon";
+        "us"; "very"; "viz"; "was"; "wasn't"; "we"; "were"; "what"; "when"; "where"; "whereby"; "wherein"; "whether";
+        "which"; "while"; "who"; "whom"; "whose"; "why"; "with"; "without"; "would"; "you"; "your" ; "have"; "thou"; "will"; 
+        "shall"
+    |] |> fun w -> new HashSet<_>(w)
+
+/// specifies whether word is noise
+let isNoiseWord (word : string) = word.Length <= 3 || noiseWords.Contains word
+
+/// Downloads and caches text files across the cluster
+let downloadAndCacheTextFiles (urls : seq<string>) : Cloud<PersistedCloudFlow<string>> =
+    CloudFlow.OfHttpFileByLine urls
+    |> CloudFlow.persist StorageLevel.Memory
+
+/// Computes the word count using the input cloud flow
+let computeWordCount (cutoff : int) (words : CloudFlow<string>) : Cloud<WordCount> =
+    words
+    |> CloudFlow.collect splitToWords
+    |> CloudFlow.map normalize
+    |> CloudFlow.filter (not << isNoiseWord)
+    |> CloudFlow.countBy id
+    |> CloudFlow.sortBy (fun (_,c) -> -c) cutoff
+    |> CloudFlow.toArray
+
+
+////////////////////////////////////////////////
+// Test the wordcount sample using textfiles.com
+
+
+// Step 1. Determine URIs to data inputs from textfiles.com
+let files = TextFiles.crawlForTextFiles() // get text file data from textfiles.com
+let testedFiles = files // |> Seq.take 50 // uncomment if you want to use a smaller subset
+
+// Step 2. Download URIs to memory of workers in cluster
+let downloadProc = cluster.CreateProcess(downloadAndCacheTextFiles testedFiles) // download text and load to cluster
+
+cluster.ShowWorkers()
+cluster.ShowProcesses()
+
+let persistedFlow = downloadProc.Result // get PersistedCloudFlow
+
+// Step 3. Perform wordcount on downloaded data
+let wordCountProc = cluster.CreateProcess(computeWordCount 100 persistedFlow) // perform the wordcount operation
+
+cluster.ShowWorkers()
+cluster.ShowProcesses()
+
+wordCountProc.Result
---
+++ HandsOnTutorial/lib/textfiles.fsx
@@ -0,0 +1,73 @@
+﻿/// Collection of utilities for downloading text files from textfiles.com
+[<RequireQualifiedAccess>]
+module TextFiles
+
+open System
+open System.Collections.Concurrent
+open System.IO
+open System.Net
+open System.Text
+open System.Text.RegularExpressions
+
+/// Crawls for text files found in textfiles.com
+let crawlForTextFilesAsync () = async {
+    let (@@) (uri : Uri) (path : string) = 
+        let ub = UriBuilder(uri)
+        ub.Path <- Path.Combine(ub.Path, path)
+        ub.Uri
+
+    let gathered = new ConcurrentDictionary<Uri, unit> ()
+    let visited = new ConcurrentDictionary<Uri, unit> ()
+    let href = new Regex("(?i)href(?-i)=\"([^\"]+)\"", RegexOptions.Compiled)
+    let getRelativeLinks (html : string) = 
+        let matches = href.Matches html
+        matches 
+        |> Seq.cast<Match> 
+        |> Seq.map (fun m -> m.Groups.[1].Value)
+        |> Seq.filter (fun m -> not (String.IsNullOrWhiteSpace m || Uri(m, UriKind.RelativeOrAbsolute).IsAbsoluteUri))
+        |> Seq.toList
+
+    let rec aux (dir : Uri) = async {
+        if not <| visited.TryAdd(dir, ()) then return () else
+        Console.WriteLine("Crawling '{0}'...", dir)
+        let wc = new WebClient()
+        let! html = wc.AsyncDownloadString(dir)
+        // text files names directories using uppercase identifiers
+        let dirs, files = getRelativeLinks html |> List.partition (fun u -> u.ToUpper() = u)
+
+        do for f in files do gathered.[dir @@ f] <- ()
+
+        return!
+            dirs
+            |> Seq.map (fun d -> aux (dir @@ d))
+            |> Async.Parallel
+            |> Async.Ignore
+    }
+
+    do! aux (Uri "http://www.textfiles.com/etext/")
+    return gathered |> Seq.map (fun kv -> kv.Key.ToString()) |> Seq.toArray
+}
+
+/// Download provided text files to local directory
+let downloadTextFilesAsync (localDir : string) (files : seq<string>) = async {
+    let download (path : string) = async {
+        let wc = new WebClient()
+        let localPath = Path.Combine(localDir, Path.GetFileName path)
+        let rec getFile i =
+            let candidate = localPath + if i = 0 then "" else sprintf "-%d" i
+            if File.Exists candidate then getFile (i+1)
+            else candidate
+
+        let localPath = getFile 0
+        do! wc.AsyncDownloadFile(Uri path, localPath)
+    }
+
+    let _ = Directory.CreateDirectory localDir
+    do! files |> Seq.map download |> Async.Parallel |> Async.Ignore
+}
+
+/// Crawls for text files found in textfiles.com
+let crawlForTextFiles () = crawlForTextFilesAsync() |> Async.RunSynchronously
+
+/// Download provided text files to local directory
+let downloadTextFiles localDir files = downloadTextFilesAsync localDir files |> Async.RunSynchronously
