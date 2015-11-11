070d728cbcf7fa011077b29235fab0332c86b9a8
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-11-11 02:58:24 +0200

    update to MBrace.Core 1.0

--- azure/create-cluster.fsx
+++ azure/create-cluster.fsx
@@ -1,11 +1,12 @@
 #load "../packages/MBrace.Azure/MBrace.Azure.fsx"
+#load "../packages/MBrace.Azure.Management/MBrace.Azure.Management.fsx"
 
-open MBrace.Azure
+open MBrace.Azure.Management
     
 let pubSettingsFile = @"... path to your downloaded publication settings file ... "
     
 // create a cluster - optional arguments include VM size, cluster size etc.
-let config = Management.CreateCluster(pubSettingsFile, Regions.North_Europe)  // adjust your region as necessary
+let config = DeploymentManager.BeginDeploy(pubSettingsFile, Regions.North_Europe, VMSizes.A3, vmCount = 4)  // adjust your region as necessary
 
 //If using the starter kit, note your connection strings and enter them in ``HandsOnTutorial/AzureCluster.fsx``
 //
@@ -19,8 +20,7 @@ let config = Management.CreateCluster(pubSettingsFile, Regions.North_Europe)  //
 //     config.ServiceBusConnectionString
 
 // Report on the status of provisioning by calling GetClusters
-Management.GetClusters(pubSettingsFile)
+DeploymentManager.ShowDeployments pubSettingsFile
 
 // Delete a cluster by supplying the cluster name
-Management.DeleteCluster(pubSettingsFile, " ... cluster name ... ")
-
+DeploymentManager.DeleteDeployment(pubSettingsFile, " ... cluster name ... ")
