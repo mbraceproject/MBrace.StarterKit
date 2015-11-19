257b4925555e1bb5b61a884198c0354d94733fc6
Don Syme <donsyme@fastmail.fm>
2015-11-19 20:17:01 +0000

    crete fix

--- HandsOnTutorial/AzureCluster.fsx
+++ HandsOnTutorial/AzureCluster.fsx
@@ -14,39 +14,22 @@ module Config =
     open MBrace.Core
     open MBrace.Runtime
     open MBrace.Azure
+    open MBrace.Azure.Management
 
-    // Both of the connection strings can be found under "Cloud Service" --> "Configure" --> scroll down to "MBraceWorkerRole"
-    //
-    // The storage connection string is of the form  
-    //    DefaultEndpointsProtocol=https;AccountName=myAccount;AccountKey=myKey 
-    //
-    // The service bus connection string is of the form
-    //    Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=%s
+    // This script is used to reconnect to your cluster.
+
+    // You can download your publication settings file at 
+    //     https://manage.windowsazure.com/publishsettings
+    let pubSettingsFile = @"C:\path\to\my.publishsettings" 
+
+    // Your cluster name is reported when you create your cluster, or can be found in 
+    // the Azure management console.
+    let clusterName = "... enter your cluster name here ..."
+
+    // Connect to the cluster 
+    let GetCluster() = 
+        let deployment = Deployment.GetDeployment(pubSettingsFile, clusterName)
+        AzureCluster.Connect(deployment.Configuration, logger = ConsoleLogger(true), logLevel = LogLevel.Info)
 
 
-    let myStorageConnectionString = "..."
-    let myServiceBusConnectionString = "..."
-
-    // Alternatively you can specify the connection strings by calling the functions below
-    //
-    // storageName: the one you specified when you created cluster.
-    // storageAccessKey: found under "Manage Access Keys" for that storage account in the Azure portal.
-    // serviceBusName: the one you specified when you created cluster.
-    // serviceBusKey: found under "Configure" for the service bus in the Azure portal
-    
-    // let createStorageConnectionString(storageName, storageAccessKey) = sprintf "DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s" storageName storageAccessKey
-    // let createServiceBusConnectionString(serviceBusName, serviceBusKey) = sprintf "Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=%s" serviceBusName serviceBusKey
-
-    let config = Configuration(myStorageConnectionString, myServiceBusConnectionString)
-
-    // It is possible to keep connection strings stored in the environment.
-    // 1. To set the environment variables:
-    // Configuration.EnvironmentStorageConnectionString <- "your storage connection string here"
-    // Configuration.EnvironmentServiceBusConnectionString <- "your service bus connection string here"
-    //
-    // 2. To recover the environment variables
-    // let config = Configuration.FromEnvironmentVariables()
-
-    let GetCluster() =
-        AzureCluster.Connect(config, logger = ConsoleLogger(true), logLevel = LogLevel.Info)
 
