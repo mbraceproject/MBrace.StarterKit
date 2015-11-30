9306c4485832b07c1b3d159f6eaaa7cf90270e7f
Isaac Abraham <isaac.abraham@gmail.com>
2015-11-30 13:27:11 +0100

    Slight improvements to dashboard.

--- HandsOnTutorial/Dashboard.fsx
+++ HandsOnTutorial/Dashboard.fsx
@@ -1,53 +1,62 @@
 ﻿(*** hide ***)
-//#load "ThespianCluster.fsx"
-#load "AzureCluster.fsx"
+#load "ThespianCluster.fsx"
+//#load "AzureCluster.fsx"
 
 #load @"..\packages\FSharp.Charting\FSharp.Charting.fsx"
 #r "../packages/FSharp.Control.AsyncSeq/lib/net45/FSharp.Control.AsyncSeq.dll"
 #load @"..\paket-files\fslaborg\FSharp.Charting\docs\content\EventEx-0.1.fsx"
+#r "PresentationCore"
+#r "PresentationFramework"
+#r "WindowsBase"
+#r "System.Xaml"
 
 open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media.Imaging
+
+open System
+open FSharp.Charting
+
 open MBrace.Core
 open MBrace.Runtime
-open FSharp.Charting
+
+
+
+let stylize chartName = Chart.WithTitle(Text = chartName, InsideArea = false)
+Window().Close()
 
 type ClusterStatusStream = IEvent<WorkerRef array>
 
-let getLiveSeries handler createChart chartName (clusterStatusStream:ClusterStatusStream) =
-    let series = clusterStatusStream |> Event.map handler
-    createChart(series, chartName)
+module Event =
+    let mapWorker getField =
+        Event.map(snd >> Seq.map(fun (worker:WorkerRef) -> worker.Id, getField worker))
+    let asTimeSeries n =
+        Event.scan(fun state ev ->
+            (state @ [ ev ])
+            |> List.skip(state.Length - n)) []
 
-let getLiveSeriesPerWorker getField = getLiveSeries (fun workers -> workers |> Seq.map(fun worker -> worker.Id, getField worker))
-
-let buildCombinedLiveChart getField createChart chartName =
-    getLiveSeriesPerWorker getField createChart chartName >> Chart.WithTitle chartName
-
-let getDefaultNullable nullable =
+module ChartBuilders =
+    let constructHistoricalSeries mapper =
+        Event.map mapper
+        >> Event.scan(fun state ev ->
+            (state @ [ ev ])
+            |> List.skip(state.Length - 60)) []
+    let asLiveChart createChart chartName stream =
+        createChart(stream, chartName) |> stylize chartName
+let private getNullable nullable =
     nullable
     |> Option.ofNullable
     |> defaultArg <| 0.
 
-let OpenDashboard (cluster:MBrace.Azure.AzureCluster) =
-    let clusterStatusStream = Event.clock 500 |> Event.map(fun _ -> cluster.Workers)
+let OpenDashboard (cluster:MBraceClient) =
+    let stream = Event.clock 500 |> Event.map(fun time -> time, cluster.Workers)
+
     Chart.Rows [
         Chart.Columns [
-            clusterStatusStream |> buildCombinedLiveChart (fun w -> w.ActiveWorkItems) (fun (a,b) -> LiveChart.Pie(a, Name = b)) "Active Work Items"
-            clusterStatusStream |> buildCombinedLiveChart (fun w -> defaultArg (w.CpuUsage |> Option.ofNullable) 0.) (fun (a,b) -> LiveChart.Column(a, Name = b) |> Chart.WithYAxis(Max = 100., Min = 0.)) "CPU Usage" ]
+            stream |> Event.mapWorker (fun worker -> worker.ActiveWorkItems) |> ChartBuilders.asLiveChart (fun (a,b) -> LiveChart.Pie(a, Name = b) |> Chart.With3D()) "Active Work Items"
+            stream |> Event.mapWorker (fun worker -> defaultArg (worker.CpuUsage |> Option.ofNullable) 0.) |> ChartBuilders.asLiveChart (fun (a,b) -> LiveChart.Column(a, Name = b) |> Chart.WithYAxis(Min = 0., Max = 100.)) "CPU Usage" ]
         Chart.Columns [
-            clusterStatusStream |> getLiveSeries (fun workers -> [ "All Workers", workers |> Seq.sumBy(fun w -> w.ActiveWorkItems) ]) (fun (a,b) -> LiveChart.Column(a, Name = b)) "Total Active Work Items"
-            clusterStatusStream |> buildCombinedLiveChart (fun w -> getDefaultNullable w.NetworkUsageDown + getDefaultNullable w.NetworkUsageUp) (fun (a,b) -> LiveChart.Bar(a, Name = b) |> Chart.WithYAxis(Min = 0., Max = 5000., Title = "Kbps")) "Bandwith Utilization" ]
+            stream |> Event.map (fun (time, workers) -> time.ToShortTimeString(), workers |> Seq.sumBy(fun w -> w.ActiveWorkItems)) |> Event.asTimeSeries 60 |> ChartBuilders.asLiveChart (fun (a, b) -> LiveChart.Line(a, Name = b)) "Total Active Work Items"
+            stream |> Event.mapWorker (fun w -> getNullable w.NetworkUsageDown + getNullable w.NetworkUsageUp) |> ChartBuilders.asLiveChart (fun (a,b) -> LiveChart.Bar(a, Name = b) |> Chart.WithYAxis(Min = 0., Max = 5000., Title = "Kbps")) "Bandwith Utilization" ]
         ]
-
-
-//let getLiveHistoricalSeries createChart getField seriesName (worker:WorkerRef) =
-//    let series =
-//        Event.clock 250
-//        |> Event.map(fun _ ->
-//            DateTime.UtcNow.ToShortTimeString(),
-//            getField worker)
-//        |> Event.scan(fun state ev -> (state @ [ ev ]) |> List.skip(state.Length - 60)) []
-//    createChart(series, seriesName + worker.WorkerId.Id)
-//
-//let buildCombinedHistoricalLiveChart createChart getField chartName workers =
-//    Chart.Combine (workers |> Array.map (getLiveHistoricalSeries createChart getField chartName))
-//    |> Chart.WithTitle chartName
